b202aa7cdb4557a8304c5cfc85e78a62d40d646c
orionfollett <orionfollett@gmail.com>
2023-08-21 12:14:16 -0400

    rename db

--- db/__init__.py
+++ db/__init__.py
@@ -2,5 +2,5 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 print("Database session starting...")
-engine = create_engine("sqlite:///db/test.db")
-Session = sessionmaker(bind=engine) 
+engine = create_engine("sqlite:///db/workouts.db")
+Session = sessionmaker(bind=engine)
